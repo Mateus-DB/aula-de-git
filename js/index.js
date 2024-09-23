3cf4ad0b3a01c584fe6719a10493b96461f1c937
mateus <demartinomateus@gmail.com>
2024-09-22 23:06:53 -0300

    fizemos um novo projeto

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
-function somar() {
-    console.log(1 + 1);
+let input = document.getElementById('senha')
+let btnIcon = document.getElementById('botao')
+
+function mudar() {
+    if (input.type == "password") {
+        input.setAttribute('type', 'text')
+        btnIcon.classList.replace('bi-eye-fill', 'bi-eye-slash-fill')
+    } else if (input.type == "text") {
+        input.setAttribute('type', 'password')
+        btnIcon.classList.replace('bi-eye-slash-fill', 'bi-eye-fill',)
+    }
+
 }
